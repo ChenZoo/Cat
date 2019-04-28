12d59724a5c6b0abbbf5e926922a0eecf5632d55
ChenZoo <31432580+ChenZoo@users.noreply.github.com>
2019-04-28 21:19:10 +0800

    Update SiameseKitten.py

--- SiameseKitten.py
+++ SiameseKitten.py
@@ -1,1 +1,17 @@
 
+
+class Cat(object):
+    
+    def __init__(self):
+        print "I am Cat!"
+        
+
+
+class SiameseKitten(Cat):
+    
+    def __init__(self):
+        super(SiameseKitten, self).__init__()
+        print "I am SiameseKitten!"
+        
+        
+SiameseKitten()
